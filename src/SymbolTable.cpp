894133543dbd30d0f03548641f923f7f716e64e6
Tyler Denniston <tyler@csail.mit.edu>
2015-02-28 09:36:46 -0500

    Updates to symbol table.

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -1,10 +1,16 @@
 #include "AST.h"
 #include "SymbolTable.h"
 
-void SymbolTable::insert(Variable *v, ASTNode *value) {
+using namespace Bish;
 
+void SymbolTable::insert(Variable *v, ASTNode *value) {
+    table[v->name] = value;
 }
 
-ASTNode *SymbolTable::lookup(Variable *v) {
-  return NULL;
+ASTNode *SymbolTable::lookup(Variable *v) const {
+    const_iterator I = table.find(v->name);
+    if (I == table.end()) {
+        return NULL;
+    }
+    return I->second;
 }
--- src/SymbolTable.h
+++ src/SymbolTable.h
@@ -1,14 +1,23 @@
 #ifndef __BISH_SYMBOL_TABLE_H__
 #define __BISH_SYMBOL_TABLE_H__
 
+#include <map>
+
+namespace Bish {
+
 class ASTNode;
 class Variable;
 
 class SymbolTable {
 public:
     void insert(Variable *v, ASTNode *value);
-    ASTNode *lookup(Variable *v);
+    ASTNode *lookup(Variable *v) const;
 private:
+    typedef std::map<std::string, ASTNode *> TableTy;
+    typedef TableTy::iterator iterator;
+    typedef TableTy::const_iterator const_iterator;
+    TableTy table;
 };
 
+}
 #endif
