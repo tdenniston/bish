d9649a9a49984a27f531a58fefc03b926e060c01
Tyler Denniston <tyler@csail.mit.edu>
2015-03-01 15:58:33 -0500

    Remove BishPrinter.

--- src/AST.h
+++ src/AST.h
@@ -124,24 +124,5 @@ private:
     ASTNode *root_;
 };
 
-class BishPrinter : public ASTVisitor {
-public:
-    BishPrinter(std::ostream &os) : stream(os), indent_level(0) {}
-    virtual void visit(const Block *);
-    virtual void visit(const Variable *);
-    virtual void visit(const IfStatement *);
-    virtual void visit(const Comparison *);
-    virtual void visit(const Assignment *);
-    virtual void visit(const BinOp *);
-    virtual void visit(const UnaryOp *);
-    virtual void visit(const Integer *);
-    virtual void visit(const Fractional *);
-    virtual void visit(const String *);
-    virtual void visit(const Boolean *);
-private:
-    std::ostream &stream;
-    unsigned indent_level;
-};
-
 }
 #endif
