21653b69f8055321086ed16cdd95203321cb087d
Tyler Denniston <tyler@csail.mit.edu>
2015-03-14 09:40:51 -0400

    Add new "TypeAnnotator" tool.
    This is primarily for debugging the type inference system.

---
+++ tools/TypeAnnotator.cpp
@@ -0,0 +1,262 @@
+#include <set>
+#include <sstream>
+#include <iostream>
+#include <cassert>
+#include "Compile.h"
+#include "IR.h"
+#include "Type.h"
+#include "Parser.h"
+
+using namespace Bish;
+
+class TypeAnnotator : public IRVisitor {
+public:
+    TypeAnnotator(std::ostream &os) : stream(os), indent_level(0) {}
+    
+    void visit(ReturnStatement *n) {
+        if (visited(n)) return;
+        visited_set.insert(n);
+        stream << "return ";
+        n->value->accept(this);
+    }
+
+    void visit(Block *n) {
+        if (visited(n)) return;
+        visited_set.insert(n);
+        stream << "{\n";
+        indent_level++;
+        for (std::vector<IRNode *>::const_iterator I = n->nodes.begin(), E = n->nodes.end();
+             I != E; ++I) {
+            indent();
+            (*I)->accept(this);
+            if (!dynamic_cast<IfStatement*>(*I) &&
+                !dynamic_cast<ForLoop*>(*I)) stream << ";\n";
+        }
+        indent_level--;
+        indent();
+        stream << "}\n\n";
+    }
+
+    void visit(Variable *n) {
+        stream << n->name << "[:" << strtype(n) << "]";
+    }
+
+    void visit(IfStatement *n) {
+        stream << "if (";
+        n->pblock->condition->accept(this);
+        stream << ") ";
+        n->pblock->body->accept(this);
+
+        for (std::vector<PredicatedBlock *>::const_iterator I = n->elses.begin(),
+                 E = n->elses.end(); I != E; ++I) {
+            indent();
+            stream << "else if (";
+            (*I)->condition->accept(this);
+            stream << ") ";
+            (*I)->body->accept(this);
+        }
+        if (n->elseblock) {
+            indent();
+            stream << "else ";
+            n->elseblock->accept(this);
+        }
+    }
+
+    void visit(ForLoop *n) {
+        stream << "for (";
+        n->variable->accept(this);
+        stream << " in ";
+        n->lower->accept(this);
+        if (n->upper) {
+            stream << " .. ";
+            n->upper->accept(this);
+        }
+        stream << ") ";
+        n->body->accept(this);
+    }
+
+    void visit(Function *n) {
+        if (visited(n)) return;
+        visited_set.insert(n);
+        stream << "def " << n->name << "(";
+        const int nargs = n->args.size();
+        int i = 0;
+        for (std::vector<Variable *>::const_iterator I = n->args.begin(),
+                 E = n->args.end(); I != E; ++I, ++i) {
+            (*I)->accept(this);
+            if (i < nargs - 1) stream << ", ";
+        }
+        stream << ") ";
+        if (n->body) n->body->accept(this);
+    }
+
+    void visit(FunctionCall *n) {
+        if (visited(n)) return;
+        visited_set.insert(n);
+        const int nargs = n->args.size();
+        stream << n->function->name << "(";
+        for (int i = 0; i < nargs; i++) {
+            n->args[i]->accept(this);
+            if (i < nargs - 1) stream << ", ";
+        }
+        stream << ")";
+    }
+
+    void visit(ExternCall *n) {
+        if (visited(n)) return;
+        visited_set.insert(n);
+        stream << "@(";
+        for (InterpolatedString::const_iterator I = n->body->begin(), E = n->body->end();
+             I != E; ++I) {
+            if ((*I).is_str()) {
+                stream << (*I).str();
+            } else {
+                assert((*I).is_var());
+                stream << "$";
+                (*I).var()->accept(this);
+            }
+        }
+        stream << ")";
+    }
+
+    void visit(IORedirection *n) {
+        if (visited(n)) return;
+        visited_set.insert(n);
+        std::string bash_op;
+        switch (n->op) {
+        case IORedirection::Pipe:
+            bash_op = "|";
+            break;
+        default:
+            assert(false && "Unimplemented redirection.");
+        }
+
+        n->a->accept(this);
+        stream << " " << bash_op << " ";
+        n->b->accept(this);
+    }
+
+    void visit(Assignment *n) {
+        if (visited(n)) return;
+        visited_set.insert(n);
+        n->variable->accept(this);
+        stream << " = ";
+        n->value->accept(this);
+    }
+
+    void visit(BinOp *n) {
+        std::string bash_op;
+        switch (n->op) {
+        case BinOp::Eq:
+            bash_op = "==";
+            break;
+        case BinOp::NotEq:
+            bash_op = "!=";
+            break;
+        case BinOp::LT:
+            bash_op = "<";
+            break;
+        case BinOp::LTE:
+            bash_op = "<=";
+            break;
+        case BinOp::GT:
+            bash_op = ">";
+            break;
+        case BinOp::GTE:
+            bash_op = ">=";
+            break;
+        case BinOp::Add:
+            bash_op = "+";
+            break;
+        case BinOp::Sub:
+            bash_op = "-";
+            break;
+        case BinOp::Mul:
+            bash_op = "*";
+            break;
+        case BinOp::Div:
+            bash_op = "/";
+            break;
+        case BinOp::Mod:
+            bash_op = "%";
+            break;
+        }
+
+        n->a->accept(this);
+        stream << " " << bash_op << " ";
+        n->b->accept(this);
+    }
+
+    void visit(UnaryOp *n) {
+        if (visited(n)) return;
+        visited_set.insert(n);
+        switch (n->op) {
+        case UnaryOp::Negate:
+            stream << "-";
+            break;
+        }
+        n->a->accept(this);
+    }
+
+    void visit(Integer *n) {
+        stream << n->value;
+    }
+
+    void visit(Fractional *n) {
+        stream << n->value;
+    }
+
+    void visit(String *n) {
+        stream << "\"" << n->value << "\"";
+    }
+
+    void visit(Boolean *n) {
+        stream << n->value;
+    }
+private:
+    unsigned indent_level;
+    std::ostream &stream;
+    std::set<IRNode *> visited_set;
+    bool visited(IRNode *n) { return visited_set.find(n) != visited_set.end(); }
+    void indent() {
+        for (unsigned i = 0; i < indent_level; i++) {
+            stream << "    ";
+        }
+    }
+    std::string strtype(IRNode *n) {
+        switch (n->type()) {
+        case UndefinedTy:
+            return "undef";
+        case IntegerTy:
+            return "int";
+        case FractionalTy:
+            return "float";
+        case StringTy:
+            return "string";
+        case BooleanTy:
+            return "bool";
+        }
+    }
+
+};
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "USAGE: " << argv[0] << " <INPUT>\n";
+        std::cerr << "  Annotates Bish file <INPUT> with inferred type information.\n";
+        return 1;
+    }
+    std::string path(argv[1]);
+
+    Parser p;
+    Module *m = p.parse(path);
+    std::stringstream s;
+    // Don't actually care about the output, just need the compile
+    // pipeline to run.
+    compile_to_bash(s, m);
+
+    TypeAnnotator annotate(std::cout);
+    m->accept(&annotate);
+
+    return 0;
+}
