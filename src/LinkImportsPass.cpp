d9c46406dec80d4e088e18989c2ed7ce0bd6653e
Tyler Denniston <tyler@csail.mit.edu>
2015-03-22 10:08:42 -0400

    Fix bug with using 'import' within a function.

--- src/LinkImportsPass.cpp
+++ src/LinkImportsPass.cpp
@@ -9,9 +9,20 @@ void LinkImportsPass::visit(Module *node) {
              E = node->global_variables.end(); I != E; ++I) {
         (*I)->accept(this);
     }
-    for (std::vector<Function *>::const_iterator I = node->functions.begin(),
-             E = node->functions.end(); I != E; ++I) {
-        (*I)->accept(this);
+    // There's probably a better way to do this. The issue is that
+    // visiting functions which have import statements in them can
+    // cause the list of functions in the module to change. Thus, we
+    // can't simply iterate over the list at this point, as it might
+    // change dynamically.
+    std::set<Function *> functions(node->functions.begin(), node->functions.end());
+    std::set<Function *> finished;
+    while (!functions.empty()) {
+        Function *f = *functions.begin();
+        functions.erase(functions.begin());
+        if (finished.find(f) != finished.end()) continue;
+        finished.insert(f);
+        f->accept(this);
+        functions.insert(node->functions.begin(), node->functions.end());
     }
 }
 
